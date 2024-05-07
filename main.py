1a33cbb48da366725da2498b6b43ab0a76a93927
taylorzhong <taylorzhong@tencent.com>
2024-05-07 19:09:22 +0800

    first init

---
+++ main.py
@@ -0,0 +1,90 @@
+
+
+# TPS = (2921.24 - 2744.70) / 2921.24 * 100
+# TPS2 = (2921.24 - 2572.09) / 2921.24 * 100
+
+# print("TPS : {} TPS2 : {}".format(TPS, TPS2))
+
+# rt_av1 = 50.42
+# rt_av2 = 363.56
+# rt_av3 = 554.89
+
+# rt_ratio1 = (rt_av2 - rt_av1) / rt_av1 * 100
+# rt_ratio2 = (rt_av3 - rt_av1) / rt_av1 * 100
+
+# print("rt_ratio1={} rt_ratio2={}".format(rt_ratio1, rt_ratio2))
+
+# rt_nine1 = 74.0
+# rt_nine2 = 1139.0
+# rt_nine3 = 1380.0
+
+# rt_np1 = (rt_nine2 - rt_nine1) / rt_nine1 * 100
+# rt_np2 = (rt_nine3 - rt_nine1) / rt_nine1 * 100
+
+# print("rt_np1={} rt_np2={}".format(rt_np1, rt_np2))
+
+# account_cpu_av1 = 46.26
+# account_cpu_av2 = 56.94
+# account_cpu_av3 = 62.82
+
+# account_cpu_ratioinc1 = (account_cpu_av2 - account_cpu_av1) / account_cpu_av1 * 100
+# account_cpu_ratioinc2 = (account_cpu_av3 - account_cpu_av1) / account_cpu_av1 * 100
+# print("account_cpu_ratioinc1={} account_cpu_ratioinc2={}".format(account_cpu_ratioinc1, account_cpu_ratioinc2))
+
+# account_cpu_ninety1 = 80.53
+# account_cpu_ninety2 = 99.92
+# account_cpu_ninety3 = 99.92
+
+# account_cpu_ratioinc_n1 = (account_cpu_ninety2 - account_cpu_ninety1) / account_cpu_ninety1 * 100
+# account_cpu_ratioinc_n2 = (account_cpu_ninety3 - account_cpu_ninety1) / account_cpu_ninety1 * 100
+# print("account_cpu_ratioinc_n1={} account_cpu_ratioinc_n2={}".format(account_cpu_ratioinc_n1, account_cpu_ratioinc_n2))
+
+# data_cpu_av1 = 1.86
+# data_cpu_av2 = 353.35
+# data_cpu_av3 = 558.22
+
+# data_cpu_av_inc1 = (data_cpu_av2 - data_cpu_av1) / data_cpu_av1 * 100
+# data_cpu_av_inc2 = (data_cpu_av3 - data_cpu_av1) / data_cpu_av1 * 100
+# print("data_cpu_av_inc1={} data_cpu_av_inc2={}".format(data_cpu_av_inc1, data_cpu_av_inc2))
+
+# data_cpu_nt1 = 3.0
+# data_cpu_nt2 = 520.71
+# data_cpu_nt3 = 765.13
+
+# data_cpu_nt_inc1 = (data_cpu_nt2 - data_cpu_nt1) / data_cpu_nt1 * 100
+# data_cpu_nt_inc2 = (data_cpu_nt3 - data_cpu_nt1) / data_cpu_nt1 * 100
+# print("data_cpu_nt_inc1={} data_cpu_nt_inc2={}".format(data_cpu_nt_inc1, data_cpu_nt_inc2))
+
+# flow_read_1 = 30443244
+# flow_read_2 = 54920096
+# flow_read_3 = 77636488
+
+# flow_write_1 = 25452708
+# flow_write_2 = 88701200
+# flow_write_3 = 135225536
+
+# flow_read_inc1 = (flow_read_2 - flow_read_1) / flow_read_1 * 100
+# flow_read_inc2 = (flow_read_3 - flow_read_1) / flow_read_1 * 100
+# print("flow_read_inc1={} flow_read_inc2={}".format(flow_read_inc1, flow_read_inc2))
+
+# flow_write_inc1 = (flow_write_2 - flow_write_1) / flow_write_1 * 100
+# flow_write_inc2 = (flow_write_3 - flow_write_1) / flow_write_1 * 100
+# print("flow_wirte_inc1={} flow_write_inc2={}".format(flow_write_inc1, flow_write_inc2))
+
+picket_read1 = 5103.96
+picket_read2 = 37643.50
+picket_read3 = 60561.70
+
+picket_write1 = 5537.89
+picket_write2 = 48732.68
+picket_write3 = 79381.59
+
+picket_read_inc1 = (picket_read2 - picket_read1) / picket_read1 * 100
+picket_read_inc2 = (picket_read3 - picket_read1) / picket_read1 * 100
+print("picket_read_inc1={} picket_read_inc2={}".format(picket_read_inc1, picket_read_inc2))
+
+picket_write_inc1 = (picket_write2 - picket_write1) / picket_write1 * 100
+picket_write_inc2 = (picket_write3 - picket_write1) / picket_write1 * 100
+print("picket_write_inc1={} picket_write_inc2={}".format(picket_write_inc1, picket_write_inc2))
+
+
